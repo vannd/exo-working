94d0991cdfc279a0750a614fda6462780005cbd9
vannd <vannd@exoplatform.com>
2014-08-11 15:58:30 +0700

    server modified testing 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 		System.out.println("Git hello world!");
 		System.out.println("Git modify test");
 		System.out.println("Server modifying ");
-		system.out.println("server modified 1");
+		System.out.println("server modified 1");
+		System.out.println("server modified 2");
 		
 	}
 }
